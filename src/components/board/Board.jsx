28cde5a23a0c168ab66e4870da5808204a70938d
santiagoTorres27 <santiago.torres27@outlook.com>
2023-10-19 01:56:55 +0200

    Board design

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Board.scss";
+import { GameContext } from "../../context/GameContext";
 
 const Board = () => {
+  const { difficulty } = useContext(GameContext);
+  const [boardStyle, setBoardStyle] = useState("");
+
+  useEffect(() => {
+    console.log(difficulty);
+    switch (difficulty) {
+      case "easy":
+        setBoardStyle("board--4x4");
+        break;
+
+      case "medium":
+        setBoardStyle("board--6x4");
+        break;
+
+      case "hard":
+        setBoardStyle("board--6x5");
+        break;
+    }
+  }, [difficulty]);
+
   return (
-    <div className="board">
+    <div className={`board ${boardStyle}`}>
       <div className="card">
         <img src="./public/img/lotr_back_card.svg" alt="" />
       </div>
